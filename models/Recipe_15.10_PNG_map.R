86581d457bd5bdcedd79be01af4555df4d9969a8
Ben Haller <github@sticksoftware.com>
2020-10-25 18:13:57 -0400

    modifications for compatibility with SLiM 3.5

--- models/Recipe_15.10_PNG_map.R
+++ models/Recipe_15.10_PNG_map.R
@@ -1,5 +1,14 @@
 # Read in a PNG map and convert it into a file Eidos can parse
 
+# BCH 10/25/2020: Note that the approach shown here, of generating a text file
+# that represents the pixels in an image, is obsolete as of SLiM 3.5.  Now it
+# is possible to simply load a PNG file with the Eidos class Image, and get a
+# channel (like the red channel used here) as a matrix that can either be used
+# directly with defineSpatialMap(), or can be modified prior to use.  So this
+# code should probably not be used by anyone ever; it's a really ugly way of
+# doing things, and there's no longer any reason to do it this way.
+
+
 library("png")
 
 i <- readPNG("Recipe_15.10_PNG_map.png", native = FALSE, info = FALSE)
